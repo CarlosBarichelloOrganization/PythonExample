137cb4d603714999d24ff4f62afd237f7aef67c6
CarlosBarichello2 <carlos@BarichelloDataScience.pep3l0pyvq4edg5pekwxx1nv5e.jx.internal.cloudapp.net>
2017-10-18 20:39:14 +0000

    Made print less formal

--- Carlos.py
+++ Carlos.py
@@ -2,4 +2,5 @@ import datetime
 
 dateFormat = '%Y-%m-%d %H:%M:%S.%f'
 currentTime = datetime.datetime.now().strftime(dateFormat)
-print "The date is: " + currentTime
+print ("Hey, the date is: " + currentTime)
+
